1cd5f82bbc568e83035c18244c66c1c0a4e530f8
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2022-05-11 02:50:55 +0600

    save model

---
+++ ckpt.py
@@ -0,0 +1,14 @@
+custom_config = {
+        "img_size": 384,
+        "in_chans": 3,
+        "patch_size": 16,
+        "embed_dim": 768,
+        "depth": 12,
+        "n_heads": 12,
+        "qkv_bias": True,
+        "mlp_ratio": 4,
+}
+
+model_custom = VisionTransformer(**custom_config)
+model_custom.eval()
+torch.save(model_custom, "model.pth")
